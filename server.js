31a5ec28cb7257e43779559c37b580f73510ae01
Santiago Gil <gil.educaciontdf@gmail.com>
2016-07-06 15:00:26 -0300

    Agrega soporte rss en server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var fs = require('fs')
 var path = require('path')
-var xtend = require('xtend')
 var hyperstream = require('hyperstream')
 var app = require('./src/app.js')
 var store = require('./src/store.js')
+var httpProxy = require('http-proxy')
+var proxy = httpProxy.createProxyServer({target: 'http://elmalvinense.com'})
+var request = require('request')
 
 var ecstatic = require('ecstatic')
 var st = ecstatic(path.join(__dirname, 'build'))
@@ -12,6 +14,13 @@ var http = require('http')
 
 var server = http.createServer(function (req, res) {
   if (req.url.match('assets')) return st(req, res)
+  if (req.url.match('api')) {
+    req.url = req.url.split('/').pop()
+    return proxy.web(req, res)
+  }
+  if (req.url.match('elmalvinense')) {
+    return request('http://elmalvinense.com/elmalvinense.xml').pipe(res) 
+  }
   var state = store({type: 'setUrl', payload: req.url})
     var elem =  app(state)
   read('index.html').pipe(hyperstream({
