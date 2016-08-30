a2d014442fe20a7d381a53571ae408dda79ddbd6
Santiago Gil <gil.educaciontdf@gmail.com>
2016-08-30 15:21:40 -0300

    Only send non redundant messages from worker

--- src/worker.js
+++ src/worker.js
@@ -3,11 +3,11 @@ var app = require('./app.js')
 
 module.exports = function (self) {
 self.addEventListener('message', function(ev){
-  // var actualstate = store(ev.data)
-  //     self.postMessage({view: app(actualstate), url: actualstate.url})
   return store(ev.data)
 })
 store.on('*', function (action, state, oldstate) {
+  if (state !== oldstate) {
   self.postMessage({view: app(state), url: state.url})
+  }
 })
 }
