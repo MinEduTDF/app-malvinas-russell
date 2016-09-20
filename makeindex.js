be4e76f7a8cee0558884d308e232bc3b788baeb1
Santiago Gil <gil.educaciontdf@gmail.com>
2016-09-20 14:50:00 -0300

    Add uuid to assets urls

---
+++ makeindex.js
@@ -0,0 +1,29 @@
+var fs = require('fs')
+
+var file = fs.createWriteStream(__dirname + '/build/index.html')
+file.end(makeindex(process.argv[2]))
+function makeindex (version) {
+return `<html><head>
+<meta charset="UTF-8">
+<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+<link rel="manifest" href="manifest.json">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="application-name" content="Malvinas">
+<meta name="apple-mobile-web-app-title" content="Malvinas">
+<meta name="msapplication-starturl" content="/">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/main.css">
+<title>Malvinas</title>
+</head>
+
+
+<body>
+<div id='main'>
+      </div>
+	<script defer src='assets/bundle-${version}.js' type='text/javascript'></script>
+  <script id='state' type='text/javascript'></script>
+</body></html>`}
