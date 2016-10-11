8669b9413031e89dac98c675a6f0f955d1fcedd5
Santiago Gil <gil.educaciontdf@gmail.com>
2016-10-11 10:40:40 -0300

    Add target blank to repo link

--- src/seccion/agradecimientos.js
+++ src/seccion/agradecimientos.js
@@ -29,7 +29,7 @@ module.exports = function noticias (state) {
     <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">Nuestras Malvinas</span> por <a xmlns:cc="http://creativecommons.org/ns#" href="https://nuestrasmalvinas.org" property="cc:attributionName" rel="cc:attributionURL">Centro de Excombatientes de Malvinas en Ushuaia y Dirección Provincial de Educación, Ciencia y Tecnología, Subsecretaría de Planeamiento Educativo, Informática y Evaluación del Ministerio de Educación de Tierra del Fuego, Antártida e Islas del Atlántico Sur</a> se distribuye bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Licencia Creative Commons Atribución-NoComercial-SinDerivar 4.0 Internacional</a>.<br />Basada en una obra en <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/MinEduTDF/app-malvinas-russell" rel="dct:source">https://github.com/MinEduTDF/app-malvinas-russell</a>.</p>
     <br/>
     <h3>C&oacute;digo fuente</h3>
-    <p><a href="https://github.com/MinEduTDF/app-malvinas-russell">Github</a></p> 
+    <p><a href="https://github.com/MinEduTDF/app-malvinas-russell" target="_blank">Github</a></p> 
     </div>`
   var fixit = state.isOpen ? style.fixit : ''
   return rv`<div>${menu(state)}
