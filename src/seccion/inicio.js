8e09583a8f67c4e668ece909f7bd4b3eb16dad2f
Santiago Gil <gil.educaciontdf@gmail.com>
2016-11-30 09:37:36 -0300

    Update cemu logo

--- src/seccion/inicio.js
+++ src/seccion/inicio.js
@@ -7,7 +7,7 @@ module.exports = function noticias (state) {
   var texto = `
 <div class="${style.center}">
   <img class="${style.logo_centro}" src="/assets/img/logo_malvinas.svg"/>
-  <img class="${style.logo_centro}" src="/assets/img/logo_centro.svg"/>
+  <img class="${style.logo_centro}" src="/assets/img/cemu3.svg"/>
 </div>
 <p>Nuestras Malvinas es el  lugar donde nos encontrarnos todos los Argentinos y Amigos de Malvinas. El fin de este sitio es recordar la escencia Argentina, recordando a los Héroes más cercanos en el tiempo, quienes han sacrificado el bien más preciado que tiene el Ser Humano, para defender la Argentinidad y mostrar al mundo el valor y cualidades superlativas de los Argentinos. Es el deseo de los creadores de este sitio, el que puedan encontrarse con su Ser Nacional y, de esta manera, una vez identificados los valores y cualidades que hacen al Argentino biennacido, poder imitarlos y replicarlos en nuestra familia y vida cotidiana, ya sea estudiando, trabajando o simplemente siendo un buen ciudadano para bien de la Nación. 
 </p><p>Esta aplicación es una idea del Centro de Excombatientes de Malvinas en Ushuaia, creado y desarrollado por Amigos de Malvinas pertenecientes a la Dirección de Ciencia y Tecnología del Ministerio de Educación de la Provincia de Tierra del Fuego.</p>
@@ -15,6 +15,6 @@ module.exports = function noticias (state) {
 <p>Bienvenidos, argentinos de todos los lugares del Mundo y de todos los tiempos, niños, jóvenes, adultos y ancianos.</p>`
   var fixit = state.isOpen ? style.fixit : ''
   return rv`<div>${menu(state)}
-	<div class='${fixit}'><div class="${style.info}"><h1 class="${style.header}">Inicio</h1>${texto}</div>
+	<div class='${fixit}'><div class="${style.info}">${texto}</div>
 	</div></div>`
 }
